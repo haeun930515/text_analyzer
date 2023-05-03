1273f8a8cd43c11d6add42d4ad40879074955d68
Haeun <haeun930515@gmail.com>
2023-05-04 03:25:58 +0900

    haeun first commit

---
+++ lib/provider/text_from_image_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+
+class TextFromImageProvider with ChangeNotifier {
+  final ImagePicker picker = ImagePicker();
+  final TextRecognizer _textRecognizer =
+      TextRecognizer(script: TextRecognitionScript.korean);
+
+  getImageFromGallery() async {
+    var picker = ImagePicker();
+    var image = await picker.pickImage(source: ImageSource.gallery);
+    var inputImage = InputImage.fromFilePath(image!.path);
+
+    String inputText = "";
+
+    TextLine line;
+
+    final recognizedText = await _textRecognizer.processImage(inputImage);
+
+    for (TextBlock block in recognizedText.blocks) {
+      for (line in block.lines) {
+        inputText += line.text;
+      }
+    }
+    print(inputText);
+  }
+}
